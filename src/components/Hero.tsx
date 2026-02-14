536b250574235556e1fe71296db81cc7f5d71452
BoldAxis0 <sdandeka@ualberta.ca>
2026-02-14 19:32:24 +0530

    finally all works!

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ export function Hero() {
     >
       <div className="max-w-4xl mx-auto flex flex-col items-center text-center">
         {/* Profile image from media folder */}
-        <div className="w-60 h-60 rounded-full overflow-hidden bg-gray-200 mb-8 flex items-center justify-center">
+        <div className="w-70 h-70 rounded-full overflow-hidden bg-gray-200 mb-8 flex items-center justify-center">
           <img
             src={profileImg}
             alt="Profile"
